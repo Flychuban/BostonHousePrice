f2ef30528f39f430a101ee6c501fcf1e7d244cad
Flychuban <kaloanas07@gmail.com>
2024-03-03 21:58:16 +0200

    add flask app api

--- DataPreparation.py
+++ DataPreparation.py
@@ -70,3 +70,22 @@ grid.predict(scaler.transform(df.iloc[0][:13].values.reshape(1, -1)))
 
 filename = 'boston_housing_model.sav'
 pickle.dump(grid, open(filename, 'wb'))
+
+# json for one instance of the data
+# {
+#     "data": {
+#         "CRIM": 0.00632,
+#         "ZN": 18.0,
+#         "INDUS": 2.31,
+#         "CHAS": 0.0,
+#         "NOX": 0.538,
+#         "RM": 6.575,
+#         "AGE": 65.2,
+#         "DIS": 4.0900,
+#         "RAD": 1.0,
+#         "TAX": 296.0,
+#         "PTRATIO": 15.3,
+#         "B": 396.90,
+#         "LSTAT": 4.98 
+#     }
+# }
--- app.py
+++ app.py
@@ -8,16 +8,23 @@ app = Flask(__name__)
 
 # Load the model
 model = pickle.load(open('boston_housing_model.sav', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb')) 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
 
 def predict_api():
     data = request.json['data']
-    prediction = model.predict([np.array(list(data.values()))])
+    
+    # Standardize the data using the scaler made in the DataPreparation.py
+    transformed_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    prediction = model.predict(transformed_data)
 
     output = prediction[0]
     return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
